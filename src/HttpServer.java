beeb4c13f10bcb66c638793c09b32ff815a8391a
Hugo van der Geest <hugo.14@live.nl>
2015-10-05 09:56:04 +0200

    afbeeldingen nog niet werkend

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -1,4 +1,7 @@
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -6,6 +9,9 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Base64;
+
+import javax.imageio.ImageIO;
 
 /**
  * De http server
@@ -43,6 +49,27 @@ public class HttpServer {
 			this.socket = socket;
 
 		}
+		
+		public void sendImage(PrintWriter writer){
+			try {
+				BufferedImage image = ImageIO.read(new File("banana.jpg"));
+				ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+				ImageIO.write(image, "jpg", outputStream);
+				
+				String byteImageString = Base64.getMimeEncoder().encodeToString(outputStream.toByteArray());
+				writer.print("HTTP/1.1 200 OK\r\n");
+				writer.print("Content-Type: image/jpeg\r\n");
+				writer.println("Content-Length: "+outputStream.toByteArray().length+"\r\n");
+				writer.print("\r\n");
+				writer.println(byteImageString);
+				//System.out.println(byteImageString);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		}
 
 		@Override
 		public void run() {
@@ -56,8 +83,12 @@ public class HttpServer {
 					output = socket.getOutputStream();
 					PrintWriter writer = new PrintWriter(output, false);
 					BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
-					System.out.println(reader.readLine());
+					boolean testImage = true;
+					boolean getRequest = false;
+					String requestType = reader.readLine();
+					if(requestType.startsWith("GET")){
+						getRequest = true;
+					}
 
 					// Hij loopt vast op de loop, maar ik weet niet waarom. Nu
 					// print hij
@@ -75,20 +106,30 @@ public class HttpServer {
 					//
 					
 					//werkende loop :)
+					
+					
 					while(reader.ready()){
 						String text = reader.readLine();
+						
 						System.out.println(text);
 						
 					}
 					
 				
 					System.out.println("buiten de loop");
-
+					if(testImage){
+						sendImage(writer);
 					
-					writer.print("HTTP/1.1 200 OK\r\n");
-					writer.print("Content-Type: text/html\r\n\r\n\r\n");
-					writer.print("<h1>Hello World</h1>\r\n");
-
+					} 
+					else if(getRequest){
+						writer.print("HTTP/1.1 200 OK\r\n");
+						writer.print("Content-Type: text/html\r\n\r\n\r\n");
+						writer.print("<h1>Hello World</h1>\r\n");
+					} else {
+						System.out.println("406");
+						writer.println("HTTP/1.1 406 Not Acceptable\r\n");
+				
+					}
 					writer.flush();
 					
 					output.close();
